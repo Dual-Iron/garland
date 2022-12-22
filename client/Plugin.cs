3c54a2184106e0b0a1a137440038d46f09d35127
Dual-Iron <31146412+Dual-Iron@users.noreply.github.com>
2022-12-22 18:45:40 -0500

    Initial commit

---
+++ client/Plugin.cs
@@ -0,0 +1,23 @@
+﻿using BepInEx;
+using BepInEx.Logging;
+using System;
+
+namespace Client;
+
+[BepInPlugin("org.ozql.garland", "Garland", "0.1.0")]
+sealed class Plugin : BaseUnityPlugin
+{
+    public static new ManualLogSource Logger { get; private set; }
+
+    public void OnEnable()
+    {
+        Logger = base.Logger;
+
+        try {
+            // Hooks go here
+        }
+        catch (Exception e) {
+            Logger.LogError(e);
+        }
+    }
+}
---
+++ common/Variables.cs
@@ -0,0 +1,8 @@
+﻿namespace Common;
+
+public static class Variables
+{
+    public const int MaxConnections = 55; // arbitrary
+    public const int Port = 10933;
+    public const string ConnectionKey = "GARLAND";
+}
